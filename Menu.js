c6d0a8039a4a93122551cb3c0e9ad0b02c14811f
dantemgs <gold_chord@mail.ru>
2018-10-24 10:26:25 +0300

    Static mhetod

--- Menu.js
+++ Menu.js
@@ -10,13 +10,15 @@ class Menu {
         for (let i = 0; i < this.items.length; i++) {
             if (this.items[i] instanceof MenuItem) {
                 result += this.items[i].render();
+            } else if (this.items[i] instanceof SubMenu){
+                result += this.items[i].render();
             }
         }
         result += `</ul>`;
         return result;
     }
 
-    removeElem(elem) {
+    static removeElem(elem) {
         elem.remove();
     }
 }
