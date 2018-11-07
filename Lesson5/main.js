319e6d3fb3039764cbc8cd33b3f289b11847aab0
dantemgs <gold_chord@mail.ru>
2018-11-07 13:37:28 +0300

    start hw5

---
+++ Lesson5/main.js
@@ -0,0 +1,14 @@
+$(document).ready(() => {
+   //Товары
+   let product1 = new Product(123, 'Ноутбук', 45600);
+   let product2 = new Product(124, 'Клавиатура', 1200);
+   let product3 = new Product(125, 'Мышь', 600);
+
+   //Корзина
+    let mycart = new Cart('getCart.json');
+
+    //Добавление товара
+    $('#products').on('click', '.buyBtn', e => {
+        mycart.addProduct(e.target);
+    })
+});
